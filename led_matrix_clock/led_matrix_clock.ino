6d3dbb594f00737215b645dad9a285a93aa0ccdd
Vitalii Shmorhun <3dvits@gmail.com>
2026-01-02 22:23:24 +0200

    Removed dependency on the MD_Parola library. Fixed a few issues. Cleaned and reorganized a bit. Implemented the text renderer in place of the MD_Parola library.

--- led_matrix_clock/led_matrix_clock.ino
+++ led_matrix_clock/led_matrix_clock.ino
@@ -7,7 +7,6 @@
 
 #include <RtcDS1302.h>
 
-#include <MD_Parola.h>     // include MajicDesigns Parola library
 #include <MD_MAX72xx.h>    // include MajicDesigns MAX72xx LED matrix library
 #include <SPI.h>           // include Arduino SPI library
 #include "clockFont.h"
@@ -28,7 +27,7 @@
 #define SET_MINUTE_BUTTON 8
 
 #define UPDATE_SEC  1000
-#define UPDATE_FAST  200
+#define UPDATE_FAST  150
 
 int currentScreenUpdateTime = UPDATE_SEC;
 OneWire oneWire(ONE_WIRE_BUS);
@@ -43,13 +42,14 @@ DallasTemperature sensors(&oneWire);
 ThreeWire myWire(4,5,2); // IO, SCLK, CE
 RtcDS1302<ThreeWire> Rtc(myWire);
 
-MD_Parola display = MD_Parola(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
+//MD_Parola display = MD_Parola(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
+MD_MAX72XX display = MD_MAX72XX(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
 
 bool tick = false;
-double screenUpdateTimer = 0;
-double termoButtonPressTimeStamp = -TERM_LOOP_DELAY;
+unsigned long  screenUpdateTimer;
+unsigned long termoButtonPressTimeStamp;
 double termoLoopTimeStamp = 0;
-double settingButtonPressTimeStamp = -TERM_LOOP_DELAY;
+unsigned long settingButtonPressTimeStamp;
 
 bool term_shown = false;
 int brightnessValue = 0;
@@ -59,7 +59,6 @@ bool set_minute_mode = false;
 
 int digi_blink_counter = 0;
 
-// Arduino setup function
 void setup(void) 
 {
     
@@ -69,26 +68,89 @@ void setup(void)
     display.setFont(mfont);
      // set the intensity (brightness) of the display (choose a number between 0 and 15)
     brightnessValue = map(analogRead(A0), 0, 950, 0, 15);
-    display.setIntensity(brightnessValue);
+    
+//    display.setIntensity(brightnessValue);
+    display.control(MD_MAX72XX::INTENSITY, brightnessValue);
     // clear the whole display
-    display.displayClear();
+    display.clear();
     pinMode(SHOW_TERM_BUTTON, INPUT_PULLUP);
     pinMode(SET_HOUR_BUTTON, INPUT_PULLUP);
     pinMode(SET_MINUTE_BUTTON, INPUT_PULLUP);
     screenUpdateTimer = millis();
     termoLoopTimeStamp = screenUpdateTimer + TERM_LOOP_DELAY;
     Rtc.Begin();
+    
 }
+
+//---------------------RENDER--------------------------
+uint16_t fontOffset(char c)
+{
+  if (c < 32 || c > 58)
+    c = 32;  // fallback -> space
+
+  uint8_t index = c - 32;
+  uint16_t offset = 0;
+
+  for (uint8_t i = 0; i < index; i++)
+  {
+    uint8_t w = pgm_read_byte(&mfont[offset]);
+    offset += w + 1;
+  }
+
+  return offset;
+}
+
+uint8_t drawChar(uint8_t col, char c)
+{
+  uint16_t off = fontOffset(c);
+  uint8_t w = pgm_read_byte(&mfont[off]);
+
+  for (uint8_t i = 0; i < w; i++)
+  {
+    uint8_t colData = pgm_read_byte(&mfont[off + 1 + i]);
+    uint8_t physCol = (MAX_DEVICES * 8 - 1) - (col + i);
+    display.setColumn(physCol, colData);
+  }
+
+  uint8_t physCol = (MAX_DEVICES * 8 - 1) - (col + w);
+  display.setColumn(physCol, 0);
+  return w + 1;
+}
+
+void drawText(const char* text, uint8_t startCol = 0)
+{
+  uint8_t col = startCol;
+
+
+  while (*text && col < MAX_DEVICES * 8)
+  {
+    col += drawChar(col, *text++);
+  }
+
+  while (col < MAX_DEVICES * 8)
+  {
+    uint8_t physCol = (MAX_DEVICES * 8 - 1) - col;
+    display.setColumn(physCol, 0);
+    col++;
+  }
+}
+
+//-------------------------------------------------------------
 
 void inc_time(int min_change = 0, int hour_change = 0)
 {
     RtcDateTime now = Rtc.GetDateTime();
     int new_min = now.Minute() + min_change;
-    if (new_min >= 60)
-        new_min = 0;
     int new_hours = now.Hour() + hour_change;
+    
+    if (new_min >= 60)
+    {
+      new_min = (new_min + 60) % 60;
+      new_hours = (new_hours + 24) % 24;
+    }
+        
     if (new_hours >= 24)
-        new_hours = 0;
+      new_hours = 0;
         
     Rtc.SetDateTime(RtcDateTime(
       now.Year(), 
@@ -107,7 +169,7 @@ void updateTime(bool no_blink = false)
         termoLoopTimeStamp = millis();
     }
     RtcDateTime now = Rtc.GetDateTime();
-    char buffer[5];
+    char buffer[6];
     sprintf(buffer, "%02d:%02d", now.Hour(), now.Minute());
     if (!no_blink)
     {
@@ -127,21 +189,21 @@ void updateTime(bool no_blink = false)
             sprintf(buffer, "%02d %02d", now.Hour(), now.Minute());
     }
     String printChar = String(buffer);
-    display.print(printChar);
+    drawText(buffer);
     tick = !tick;
     
 }
 
 void updateTemp(void)
 {
-    sensors.requestTemperatures(); 
-    char result[8]; 
+    sensors.requestTemperatures();
+    char result[6]; 
     dtostrf(sensors.getTempCByIndex(0), 2, 1, result); 
-    display.print(result);
+    strcat(result, "!"); 
+    drawText(result);
     term_shown = true;
 }
 
-// Arduino main loop function
 void loop(void) 
 { 
       
@@ -160,18 +222,19 @@ void loop(void)
         while(!digitalRead(SET_HOUR_BUTTON))
         {
             delay(1);
-//            long_press_counter++;
-//            if (long_press_counter > BUTTON_LONG_PRESS_TIMEOUT)
-//            {
-//                inc_time(0,1);
-//                updateTime(true);
-//                delay(250);
-//            }
+            long_press_counter++;
+            if (long_press_counter > BUTTON_LONG_PRESS_TIMEOUT)
+            {
+                inc_time(0,1);
+                updateTime(true);
+                delay(150);
+            }
         }
+        if (set_hour_mode)
+            inc_time(0,1);
         set_hour_mode = true;
         set_minute_mode = false;
         setButtonJustPressed = true;
-        inc_time(0,1);
     }
     if (!digitalRead(SET_MINUTE_BUTTON))
     {
@@ -180,18 +243,20 @@ void loop(void)
         while(!digitalRead(SET_MINUTE_BUTTON))
         {
             delay(1);
-//            long_press_counter++;
-//            if (long_press_counter > BUTTON_LONG_PRESS_TIMEOUT)
-//            {
-//                inc_time(1,0);
-//                updateTime(true);
-//                delay(250);
-//            }
+            long_press_counter++;
+            if (long_press_counter > BUTTON_LONG_PRESS_TIMEOUT)
+            {
+                inc_time(1,0);
+                updateTime(true);
+                delay(150);
+            }
         }
+        if (set_minute_mode)
+            inc_time(1,0); 
         set_minute_mode = true;
         set_hour_mode = false;
         setButtonJustPressed = true;
-        inc_time(1,0); 
+        
     }
     if (setButtonJustPressed)
     {
@@ -208,22 +273,25 @@ void loop(void)
         currentScreenUpdateTime = UPDATE_SEC;
 
     int currentBrightnessValue = map(analogRead(A0), 0, 950, 0, 15);
-    if (currentBrightnessValue != brightnessValue)
+    if (abs(currentBrightnessValue - brightnessValue) > 5)
     {
         brightnessValue = currentBrightnessValue;
-        display.setIntensity(brightnessValue);
+        display.control(MD_MAX72XX::INTENSITY, brightnessValue);
     }
     if (!digitalRead(SHOW_TERM_BUTTON))
     {
-        delay(10);
+        char buffer[6];
+        sprintf(buffer, "&");
+        drawText(buffer, 0);
+        
+        delay(2);
+
         while(!digitalRead(SHOW_TERM_BUTTON))
             delay(1);
         termoButtonPressTimeStamp = millis();
         set_hour_mode = false;
         set_minute_mode = false;
     }
-    
-    
     if (currentMillis - screenUpdateTimer > currentScreenUpdateTime)
     {
         bool show_term = false;
